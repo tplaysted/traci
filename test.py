c7c9c3cf44ecaf8fe647138202baedf776dc5451
tplaysted <t.playsted@gmail.com>
2024-01-21 15:29:41 +1100

    Run baseline testing on cbd scenario

--- test.py
+++ test.py
@@ -133,8 +133,8 @@ if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'neat/config-ctrnn')
 
-    get_genome_stats(config_path, w_path='neat/grid/winner-genome-2',  output_path=None, n=1000)
-    # get_baseline_stats(n=1000, output_path=None)
+    # get_genome_stats(config_path, w_path='neat/grid/winner-genome-2',  output_path=None, n=1000)
+    get_baseline_stats(n=1000, output_path=None)
 
     # print(test_winner(config_file=config_path, w_path='neat/grid/winner-genome-2'))
     # print(test_baseline())
